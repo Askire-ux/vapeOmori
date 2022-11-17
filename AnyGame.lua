5357583d20ac54b6f0d530b7af7a3b73ab09e23c
outgoinq <askireyoshirou@gmail.com>
2022-11-17 03:48:44 +0000

    cat

--- AnyGame.lua
+++ AnyGame.lua
@@ -4330,7 +4330,7 @@ runcode(function()
 						repeat
 							if ChatSpammer["Enabled"] then
 								pcall(function()
-									repstorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer((#ChatSpammerMessages["ObjectList"] > 0 and ChatSpammerMessages["ObjectList"][math.random(1, #ChatSpammerMessages["ObjectList"])] or "vxpe on top"), "All")
+									repstorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer((#ChatSpammerMessages["ObjectList"] > 0 and ChatSpammerMessages["ObjectList"][math.random(1, #ChatSpammerMessages["ObjectList"])] or "mreow meowww meow mreeoooowww"), "All")
 								end)
 							end
 							if waitnum ~= 0 then
